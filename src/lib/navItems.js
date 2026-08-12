de690d2576e0b0fad44894873514c598397a1177
fxjakaria04 <freelancerx2023@gmail.com>
2026-08-12 13:16:38 +0600

    fixed

--- src/lib/navItems.js
+++ src/lib/navItems.js
@@ -1,7 +1,7 @@
 export const navItems = [
   { id: 1, name: "Home", href: "/#home" },
   { id: 2, name: "About", href: "/#about" },
-  { id: 3, name: "Projects", href: "/#projects" },
+  { id: 3, name: "Projects", href: "/projects" },
   { id: 4, name: "Skills", href: "/#skills" },
   { id: 5, name: "Experience", href: "/#experience" },
   { id: 6, name: "Contact", href: "/#contact" },
