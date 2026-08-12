dbd502ecd6e6e5785a7d4d29a9383c72aa5745f7
fxjakaria04 <freelancerx2023@gmail.com>
2026-08-12 11:49:02 +0600

    fixed

--- src/app/page.js
+++ src/app/page.js
@@ -10,9 +10,9 @@ export default function Home() {
     <>
       <Hero />
       <About />
+      <Experience />
       <Projects />
       <Skills />
-      <Experience />
       <GetInTouch />
     </>
   );
